2910593898a77667cf180705d7fdd8f748bd3c4b
zhangxi <zhangxi_my@sina.cn>
2019-04-04 22:11:32 +0800

    add Gps file

---
+++ Gps.java
@@ -0,0 +1,36 @@
+package map.baidu.com.baidumap.Utils;
+
+/**
+ * Created by Administrator on 2017/4/14.
+ */
+
+public class Gps {
+    private double wgLat;
+    private double wgLon;
+
+    public Gps(double wgLat, double wgLon) {
+        setWgLat(wgLat);
+        setWgLon(wgLon);
+    }
+
+    public double getWgLat() {
+        return wgLat;
+    }
+
+    public void setWgLat(double wgLat) {
+        this.wgLat = wgLat;
+    }
+
+    public double getWgLon() {
+        return wgLon;
+    }
+
+    public void setWgLon(double wgLon) {
+        this.wgLon = wgLon;
+    }
+
+    @Override
+    public String toString() {
+        return wgLat + "," + wgLon;
+    }
+}
